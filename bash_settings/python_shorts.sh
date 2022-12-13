7fde4597c61abb03f362522acdf9f0dff65a0df9
Shanu Stephen <stephen.shanu@gmail.com>
2022-12-12 17:13:01 -0700

    Installing and setting virtual environment for python

---
+++ bash_settings/python_shorts.sh
@@ -0,0 +1,7 @@
+# sudo apt-get install python3.8
+# sudo apt-get install python3.8-venv
+# python3.8 -m pip install --upgrade pip
+# python -m venv ~/envs/py38
+source ~/envs/py38/bin/activate
+alias python="python3.8"
+alias pip="pip3"
